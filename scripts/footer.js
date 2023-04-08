372d68f8797ca29ee33da7b99546d5d8a8eab8db
Juan Carlos Barrios <tornillosbabe@hotmail.com>
2023-04-07 21:46:06 -0600

    creacion de footer

---
+++ scripts/footer.js
@@ -0,0 +1,10 @@
+const body = document.querySelector('body')
+const footer = 
+  `<footer>
+  <div class="container">
+    <p>FOODHUD THEME BY MODELTHEME</p>
+    <P>ALL RIGHT RESERVED</P>
+  </div>
+  </footer>`
+
+  body.insertAdjacentHTML('beforeend', footer)
